2b2c79a8bb1d83ef8b5428ff1dcc8b9cb6b8451f
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-12-04 10:12:56 -0500

    feat: add auth/no-auth components

---
+++ src/app/_utils/withAuth.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { AuthContext } from '@/app/_contexts/auth'
+
+export default function withAuth(Component) {
+  return function WithAuth(props) {
+    const { isAuthenticated } = useContext(AuthContext)
+    const [isLoading, setIsLoading] = useState(true)
+    const router = useRouter()
+
+    useEffect(() => {
+      if (!isAuthenticated()) {
+        router.push('/')
+      } else {
+        setIsLoading(false)
+      }
+    }, [isAuthenticated, router])
+
+    if (isLoading) {
+      return null
+    }
+
+    return <Component {...props} />
+  }
+}
