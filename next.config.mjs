d6312c39686cf3fc7f739b09a67d0655eb9a0edc
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-12-12 19:54:59 -0500

    fix: update api method to avoid preflight

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['example.com']
   },
@@ -11,7 +11,15 @@ const nextConfig = {
     })
     return config
   },
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*'
+      }
+    ]
+  }
 }
 
 export default nextConfig
